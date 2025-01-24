0440c691d5299601d5c951fea76bcc9fed506408
Srishti Tiwari <86340352+srishtiwari-9@users.noreply.github.com>
2025-01-24 13:27:02 +0530

    Added Spring Boot File
    This is the palindrome checker using Spring Boot. I haven't added the whole application, just the main code file.

--- Palindrome Checker/PalindromeChecker.java
+++ Palindrome Checker/PalindromeChecker.java
@@ -1,35 +1,22 @@
-//Palindrome Code
-import java.util.*;
+package com.palin.Palindrome;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/palindrome")
 public class PalindromeChecker {
 
-    public static void main(String[] args) 
-    {
-        // takes input
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string to check if it's a palindrome: ");
-        String input = sc.nextLine();
-
-        // checking method
-        boolean isPalindrome = checkPalin(input);
-
-        // display result
-        if (isPalindrome)
-            System.out.println("The string '" + input + "' is a palindrome.");
-        else
-            System.out.println("The string '" + input + "' is not a palindrome.");
-
-        sc.close();
-    }
-
-    private static boolean checkPalin(String input) {
-        // Remove non-alphanumeric characters and convert to lowercase
+    @GetMapping("/check")
+    public String checkPalindrome(@RequestParam String input) {
         String str = input.toLowerCase();
+        String rev = new StringBuilder(str).reverse().toString();
 
-        // Reverse the string
-        String reversed = new StringBuilder(str).reverse().toString();
-
-        // Compare the original and reversed strings
-        return str.equals(reversed);
+        if (str.equals(rev))
+            return "The string \"" + input + "\" is a palindrome.";
+        else
+            return "The string \"" + input + "\" is not a palindrome.";
     }
 }
